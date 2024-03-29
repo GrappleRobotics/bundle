046ce622207639003f1cb6c550a27566f7890f21
Jaci Brunning <jaci.brunning@gmail.com>
2024-03-29 14:00:32 +1100

    Zip up the SVDs

--- src/resources.rs
+++ src/resources.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read};
 
 use rust_embed::RustEmbed;
 use serde::Deserialize;
+use zip::ZipArchive;
 
 #[derive(RustEmbed)]
 #[folder = "resources"]
@@ -31,10 +32,14 @@ pub struct Algo {
 lazy_static::lazy_static! {
   pub static ref ALGOS: Vec<(regex::Regex, Algo, svd_parser::svd::Device)> = {
     let mut m = Vec::new();
+    let svd_resource = Resources::get("svd.zip").unwrap();
+    let mut zip = ZipArchive::new(std::io::Cursor::new(&svd_resource.data[..])).unwrap();
     for resource in Resources::iter().filter(|x| x.starts_with("algo/")) {
       let resource = Resources::get(&resource).unwrap();
       let algo: Algo = serde_json::from_slice(&resource.data[..]).unwrap();
-      let svd = svd_parser::parse(&String::from_utf8_lossy(&Resources::get(&algo.svd).unwrap().data)).unwrap();
+      let mut s = String::new();
+      zip.by_name(&algo.svd).unwrap().read_to_string(&mut s).unwrap();
+      let svd = svd_parser::parse(&s).unwrap();
       m.push((regex::Regex::new(&algo.pattern).unwrap(), algo, svd));
     }
     m
